a1fa420c78c9cfd94854bcb448b0d8cbab33ff78
shivam7374 <shivamsingla491@gmail.com>
2023-01-12 01:15:51 +0530

    Minor Changes

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
-export const BACKEND_URL = "https://web-production-f947.up.railway.app";
+// export const BACKEND_URL = "https://web-production-f947.up.railway.app";
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 export const TRANSACTION_CATEGORIES = [
     "clothing",
     "food",
